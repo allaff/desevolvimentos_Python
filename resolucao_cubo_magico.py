febc9e0d072abb9c1a6f869de9712cb37ff25ee5
Ginaldo Ribeiro <ginaldo.ribeiro@gmail.com>
2022-05-12 21:40:11 -0300

    implementação teste version 1.0

---
+++ resolucao_cubo_magico.py
@@ -0,0 +1,11 @@
+from time import *
+cubo = []
+
+with open("cubo2x2.txt") as arquivo:
+    
+    texto = arquivo.readline()
+    for linha in texto:
+        sleep(0.05)
+        cubo.append(linha)
+
+print(cubo)
