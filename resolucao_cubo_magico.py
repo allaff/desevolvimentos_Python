a66b8919f7d4bf592ca35b69f738e1a58e826d75
Ginaldo Ribeiro <ginaldo.ribeiro@gmail.com>
2022-05-12 21:42:10 -0300

    add .strip

--- resolucao_cubo_magico.py
+++ resolucao_cubo_magico.py
@@ -6,6 +6,6 @@ with open("cubo2x2.txt") as arquivo:
     texto = arquivo.readline()
     for linha in texto:
         sleep(0.05)
-        cubo.append(linha)
+        cubo.append(linha.strip)
 
 print(cubo)
